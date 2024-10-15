b769021fc95b6ab1d124efd15cfc57320d4e9500
booi0003 <booi0003@student.monash.edu>
2024-10-15 20:13:12 +0800

    Added csv for tripadvisor dataset and miscellaneous fixes

--- homework week 9/vega_lite_vis.js
+++ homework week 9/vega_lite_vis.js
@@ -1,10 +1,10 @@
 var vg_1 = "homework week 9/visualization.vl.json";
-var vg_2 = "homework week 9/bar_chart.json";
+var vg_2 = "homework week 9/bar_chart_google.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#barchart", vg_2).then(function(result) {
+vegaEmbed("#bar_chart_google", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
