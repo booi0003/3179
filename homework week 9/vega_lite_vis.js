8b7ba9fa03d0c9537129cbadccc1c704c8e91e7b
booi0003 <booi0003@student.monash.edu>
2024-10-15 22:09:21 +0800

    Modified HTML and VegaLite Map

--- homework week 9/vega_lite_vis.js
+++ homework week 9/vega_lite_vis.js
@@ -1,6 +1,8 @@
 var vg_1 = "homework week 9/visualization.vl.json";
 var vg_2 = "homework week 9/bar_chart_google.json";
 var vg_3 = "homework week 9/bar_chart_tripadvisor.json";
+var vg_4 = "homework week 9/donut_google.json";
+var vg_5 = "homework week 9/donut_tripadvisor.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -11,5 +13,13 @@ vegaEmbed("#bar_chart_google", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#bar_chart_tripadvisor", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#donut_google", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#donut_tripadvisor", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
