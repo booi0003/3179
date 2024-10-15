f538ed27cc2d70833d6dd96a4eeb80ab1bc0ae51
booi0003 <booi0003@student.monash.edu>
2024-10-15 21:26:24 +0800

    Added donut charts

--- homework week 9/vega_lite_vis.js
+++ homework week 9/vega_lite_vis.js
@@ -1,10 +1,15 @@
 var vg_1 = "homework week 9/visualization.vl.json";
 var vg_2 = "homework week 9/bar_chart_google.json";
+var vg_3 = "homework week 9/bar_chart_tripadvisor.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed("#bar_chart_google", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar_chart_tripadvisor", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
